62739e9b36a08008584be3b331c49e566ee225d4
Priyesh Singh <priyeshsingh30@gmail.com>
2021-06-13 10:48:11 +0530

    Test cmmit

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
